a0fa2b94cbf247a818ffe5bbd6a86d4ea6706028
Elsa Wide <elsa.wide@outlook.com>
2021-11-01 12:34:10 +0100

    cant uppload image without a name, only have to fill in name first time you save a image.

--- app/src/main/java/com/example/drawableapp/Art.java
+++ app/src/main/java/com/example/drawableapp/Art.java
@@ -14,6 +14,7 @@ import androidx.annotation.Nullable;
 import java.util.ArrayList;
 
 public class Art extends ImageView {
+	private String name;
 	private Paint pen = new Paint();
 	private Path path;
 	private ArrayList<Path> paths = new ArrayList<Path>();
@@ -43,10 +44,15 @@ public class Art extends ImageView {
 		this.pen.setStrokeCap(Paint.Cap.ROUND);
 		this.pen.setStrokeJoin(Paint.Join.ROUND);
 		this.pen.setStrokeWidth(10.0f);
+		this.name = "";
 	}
 
-	public ArrayList<Path> getPaths(){
-		return paths;
+	public String getName(){
+		return name;
+	}
+
+	public void setName(String name){
+		this.name = name;
 	}
 
 	// Purpose: This method is called when the object is touched. (Someone is attempting to draw.)
--- app/src/main/java/com/example/drawableapp/DrawActivity.java
+++ app/src/main/java/com/example/drawableapp/DrawActivity.java
@@ -3,7 +3,6 @@ package com.example.drawableapp;
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,7 +11,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,11 +28,10 @@ public class DrawActivity extends AppCompatActivity {
 	private ImageButton penButton;
 	private ImageButton eraserButton;
 	private ImageButton saveButton;
-	private ImageButton test;
-	private Art imgView;
+	private Art projectArt;
 
 	FirebaseStorage storage = FirebaseStorage.getInstance();
-	//StorageReference storageRef = storage.getReferenceFromUrl("gs://testfordatabase-8081f.appspot.com");
+	StorageReference storageRef = storage.getReference();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -48,9 +45,9 @@ public class DrawActivity extends AppCompatActivity {
 		//this.art = (Art) this.findViewById(R.id.art);
 		this.penButton = (ImageButton) this.findViewById(R.id.penButton);
 		this.eraserButton = (ImageButton) this.findViewById(R.id.eraserButton);
-		this.saveButton = (ImageButton) this.findViewById(R.id.menuButton);
+		this.saveButton = (ImageButton) this.findViewById(R.id.save_button);
 
-		imgView = (Art) findViewById(R.id.art);
+		projectArt = (Art) findViewById(R.id.art);
 
 		ImageButton backButton = (ImageButton) this.findViewById(R.id.backButton);
 		backButton.setOnClickListener(new View.OnClickListener() {
@@ -62,18 +59,19 @@ public class DrawActivity extends AppCompatActivity {
 			}
 		});
 
-		//imgView.setDrawingCacheEnabled(true);
-		//imgView.buildDrawingCache();
-		//Bitmap b = imgView.getDrawingCache();
-		//test = findViewById(R.id.penButton);
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				//penButton.setImageBitmap(b);
-				//Log.i("info",storage.toString());
-				//imgView = (View) findViewById(R.id.art);
-				saveProjectDialog();
+
+				//kollar om project redan har ett namn och sparar isåfall över den filen
+				//annars öppnar den en dialog för att spara ett projekt för första gången
+				if(projectArt.getName().isEmpty()){
+					saveProjectDialog();
+				} else {
+					uploadImage(projectArt.getName());
+				}
+
 			}
 		});
 	}
@@ -82,18 +80,19 @@ public class DrawActivity extends AppCompatActivity {
 		Dialog dialog = new Dialog(this);
 		dialog.setContentView(R.layout.view_save_project);
 
-
 		Button cancelBtn = dialog.findViewById(R.id.cancel_button);
 		Button saveBtn = dialog.findViewById(R.id.save_button);
-
+		EditText nameEditText = dialog.findViewById(R.id.name_edit_text);
 
 		saveBtn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
 
-				EditText nameEditText = dialog.findViewById(R.id.name_edit_text);
+
 				String name = nameEditText.getText().toString();
+
 				if(!name.isEmpty()) {
+					projectArt.setName(name);
 					uploadImage(name);
 					dialog.dismiss();
 				}
@@ -110,24 +109,20 @@ public class DrawActivity extends AppCompatActivity {
 	}
 
 	public void uploadImage(String name){
-		// Create a storage reference from our app
-		StorageReference storageRef = storage.getReference();
-
-// Create a reference to "mountains.jpg"
+		// Create a reference to "mountains.jpg"
 		StorageReference projectRef = storageRef.child(name + ".jpg");
 
-// Create a reference to 'images/mountains.jpg'
-		StorageReference mountainImagesRef = storageRef.child("images/" + name + ".jpg");
+		//ändra art viewn till en jpeg
+		projectArt.setDrawingCacheEnabled(true);
+		projectArt.buildDrawingCache();
 
-
-		imgView.setDrawingCacheEnabled(true);
-		imgView.buildDrawingCache();
-		Bitmap bitmap = imgView.getDrawingCache();//((BitmapDrawable) imgView.getDrawable()).getBitmap();
+		Bitmap bitmap = projectArt.getDrawingCache();
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
 		byte[] data = baos.toByteArray();
 
-		projectExist(projectRef);
+
+		//ladda upp bilden
 		UploadTask uploadTask = projectRef.putBytes(data);
 		uploadTask.addOnFailureListener(new OnFailureListener() {
 			@Override
@@ -141,23 +136,44 @@ public class DrawActivity extends AppCompatActivity {
 				baos.reset();
 				// taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
 				// ...
-				imgView.setDrawingCacheEnabled(false);
+				projectArt.setDrawingCacheEnabled(false);
 			}
 		});
 	}
 
-	public void projectExist(StorageReference storageRef){
-		storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+	public void deleteProject(String name){
+		StorageReference projectRef = storageRef.child(name +".jpg");
+
+// Delete the file
+		projectRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
 			@Override
-			public void onSuccess(Uri uri) {
-				Log.i("info","file exist");
-				// Got the download URL for 'users/me/profile.png'
+			public void onSuccess(Void aVoid) {
+				// File deleted successfully
 			}
 		}).addOnFailureListener(new OnFailureListener() {
 			@Override
 			public void onFailure(@NonNull Exception exception) {
-				// File not found
+				// Uh-oh, an error occurred!
+			}
+		});
+	}
+
+
+	public void setNameExist(String name){
+
+		StorageReference projectRef = storageRef.child(name + ".jpg");
+		projectRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+			@Override
+			public void onSuccess(Uri uri) {
+				Log.i("info","file exist");
+
+			}
+		}).addOnFailureListener(new OnFailureListener() {
+			@Override
+			public void onFailure(@NonNull Exception exception) {
+
 				Log.i("info","file not found");
+
 			}
 		});
 	}
@@ -176,4 +192,3 @@ public class DrawActivity extends AppCompatActivity {
 		this.art.selectEraser();
 	}
 }
-
